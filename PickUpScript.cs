cc4988d642153a99291c94f15632f3768ce281c7
JonDevTutorial <104003356+JonDevTutorial@users.noreply.github.com>
2022-04-19 12:34:50 +0100

    NonCommented

---
+++ PickUpScript.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpScript : MonoBehaviour
+{
+    public GameObject player;
+    public Transform holdPos;
+
+    public float pickUpRange = 5f;
+    private float rotationSensitivity = 1f;
+    private GameObject heldObj;
+    private Rigidbody heldObjRb;
+    private bool canThrow = true;
+    private int LayerNumber;
+
+    //Reference to script which includes mouse movement of player (looking around)
+    //we want to disable the player looking around when rotating the object
+    //example below 
+    //MouseLookScript mouseLookScript;
+    void Start()
+    {
+        LayerNumber = LayerMask.NameToLayer("holdLayer");
+
+        //mouseLookScript = player.GetComponent<MouseLookScript>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (heldObj == null)
+            {
+                print(LayerNumber);
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickUpRange))
+                {
+                    if (hit.transform.gameObject.tag == "canPickUp")
+                    {
+                        PickUpObject(hit.transform.gameObject);
+                    }
+                }
+            }
+            else
+            {
+                StopClipping();
+                DropObject();
+            }
+        }
+        if (heldObj != null)
+        {
+            MoveObject();
+            RotateObject();
+            if (Input.GetKeyDown(KeyCode.Mouse0) && canThrow == true)
+            {
+                StopClipping();
+                ThrowObject();
+            }
+
+        }
+    }
+    void PickUpObject(GameObject pickUpObj)
+    {
+        if (pickUpObj.GetComponent<Rigidbody>())
+        {
+            heldObj = pickUpObj;
+            heldObjRb = pickUpObj.GetComponent<Rigidbody>();
+            heldObjRb.isKinematic = true;
+            heldObjRb.transform.parent = holdPos.transform;
+            heldObj.layer = LayerNumber;
+            Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), true);
+        }
+    }
+    void DropObject()
+    {
+        Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
+        heldObj.layer = 0;
+        heldObjRb.isKinematic = false;
+        heldObj.transform.parent = null;
+        heldObj = null;
+    }
+    void MoveObject()
+    {
+        heldObj.transform.position = holdPos.transform.position;
+    }
+
+
+    void RotateObject()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            canThrow = false;
+            //disable player being able to look around
+
+            //mouseLookScript.verticalSensitivity = 0f;
+            //mouseLookScript.lateralSensitivity = 0f;
+            float XaxisRotation = Input.GetAxis("Mouse X") * rotationSensitivity;
+            float YaxisRotation = Input.GetAxis("Mouse Y") * rotationSensitivity;
+
+            heldObj.transform.Rotate(Vector3.down, XaxisRotation);
+            heldObj.transform.Rotate(Vector3.right, YaxisRotation);
+        }
+        else
+        {
+            //mouseLookScript.verticalSensitivity = originalvalue;
+            //mouseLookScript.lateralSensitivity = originalvalue;
+            canThrow = true;
+        }
+    }
+    void ThrowObject()
+    {
+
+        Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
+        heldObj.layer = 0;
+        heldObjRb.isKinematic = false;
+        heldObjRb.AddForce(transform.forward * 1000);
+        heldObj.transform.parent = null;
+        heldObj = null;
+    }
+    void StopClipping()
+    {
+        var clipRange = Vector3.Distance(heldObj.transform.position, transform.position);
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, transform.TransformDirection(Vector3.forward), clipRange);
+
+        if (hits.Length > 1)
+        {
+            heldObj.transform.position = transform.position + new Vector3(0f, -0.5f, 0f);
+            print("clip prevented");
+        }
+    }
+}
